34791fd6f1f41ee8fc0b26641c2af0f2fb59e8be
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2023-05-30 18:00:34 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import Master from "./layouts/master";
+import Master from "./frontend/layouts/master";
 
 const App: React.FC = () => {
   return (
